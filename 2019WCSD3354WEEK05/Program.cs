32fd46d1ab889fa9201a99aab5dd44d5926be789
visualstudio <visualstudio@230-PC33>
2019-02-07 15:16:12 -0500

    at 122

--- 2019WCSD3354WEEK05/Program.cs
+++ 2019WCSD3354WEEK05/Program.cs
@@ -6,34 +6,121 @@ using System.Threading.Tasks;
 
 namespace _2019WCSD3354WEEK05
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
-            var a = new testques2();
-            a.Play();
+
+            var a = new TestQuestion2();
+
+            a.PlayingWithForLoops();
+
         }
+
     }
 
-    class testques2
+
+
+    class TestQuestion2
+
     {
-        public int myfavvari = 0;
-        public void Play()
-        {
-            //write a for loop
 
-            for (; my() ;)
-            {
-                if (myfavvari <=10 )
-                Console.WriteLine("you are fool");
-                break;
-            }
-            Console.WriteLine("shit for loop again");
-        }
-        public bool my()
+        public int myFavoriteVariable = 0;
+
+
+
+        public void PlayingWithForLoops()
+
         {
-            myfavvari++;
-            return true;
+
+            // write a For Loop to Add 10 Numbers
+
+            while (MyMethod())
+
+            {
+
+                if (myFavoriteVariable > 10)
+
+                {
+
+                    Console.WriteLine("i am so out of here!");
+
+                    break;
+
+                }
+
+                Console.WriteLine("oh no I have to go through this stupid loop again...");
+
+            }
+
         }
+
+        public bool MyMethod()
+
+        {
+
+            myFavoriteVariable++;
+
+            return true;
+
+        }
+
     }
+
+
+
+    class dog
+
+    {
+
+        public dog(string name, string breed)
+
+        {
+
+            dog_name = name;
+
+            dog_breed = breed;
+
+        }
+
+
+
+        public string dog_name;
+
+        public string dog_breed;
+
+        public dog next_dog;
+
+        public dog prev_dog;
+
+
+
+    }
+
+    class birthday_party
+
+    {
+
+        public dog peanut;
+
+        public dog fifi;
+
+        public dog clarence;
+
+        public dog roy;
+
+
+
+        public dog head;
+
+        public dog tail;
+
+        public dog temporary;
+
+    }
+
 }
