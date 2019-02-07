2b0a166b8a525c17a68edeb629dfc9baea40dcf1
visualstudio <visualstudio@230-PC33>
2019-02-07 13:58:45 -0500

    at 78

---
+++ 2019WCSD3354WEEK05/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019WCSD3354WEEK05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new testques2();
+            a.Play();
+        }
+    }
+
+    class testques2
+    {
+        public void Play()
+        {
+            //write a for loop
+
+            for (; my() ;)
+            {
+                Console.WriteLine("you are fool");
+            }
+
+        }
+        public bool my()
+        {
+            return true;
+        }
+    }
+}
